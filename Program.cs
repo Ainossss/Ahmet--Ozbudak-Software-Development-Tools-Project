4fdc57cfa6b0625e8f5257be3de373c332feb170
Ahmet Ozbudak <hmtzbdk@gmail.com>
2025-12-03 21:37:01 +0100

    Added simple array and printed elements

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 3, 7, 1, 9, 4 };
+
+        Console.WriteLine("Elements of Array:");
+        foreach (var num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
